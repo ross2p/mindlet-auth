62bd50246581fd589942a4e092152b4a345aa05f
ross2p <rostyslavpasternak@gmail.com>
2026-01-21 20:21:11 +0200

    feat: restructure authentication and session management modules, add new controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '@ross2p/database';
-import { AuthModule } from './modules/auth/auth.module';
-import { CredentialsModule } from './modules/credentials/credentials.module';
-import { GoogleModule } from './modules/google/google.module';
-import { UserValidatorModule } from './modules/user-validator/user-validator.module';
-import { SessionModule } from './modules/session/session.module';
+import { AuthModule } from './auth/auth.module';
+import { CredentialsModule } from './credentials/credentials.module';
+import { GoogleModule } from './google/google.module';
+import { UserValidatorModule } from './user-validator/user-validator.module';
+import { SessionModule } from './session/session.module';
 import { RouterModule } from '@nestjs/core';
 
 @Module({
